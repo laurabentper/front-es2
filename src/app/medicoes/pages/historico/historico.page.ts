e994793b06235543582eda481f2b356f8b87552b
Laura Perlingeiro <laura.bentper@gmail.com>
2026-06-15 02:08:33 -0300

    feat(medicoes): listar historico de medicoes

--- src/app/medicoes/pages/historico/historico.page.ts
+++ src/app/medicoes/pages/historico/historico.page.ts
@@ -1,20 +1,56 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { DatePipe } from '@angular/common';
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardTitle,
+  IonContent,
+  IonText,
+} from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
+
+import { RelatorioService } from '../../../relatorios/services/relatorio.service';
+import { MedicaoResposta } from '../../models/medicao-resposta.model';
 
 @Component({
   selector: 'app-historico',
   templateUrl: './historico.page.html',
   styleUrls: ['./historico.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    DatePipe,
+    RouterLink,
+    IonContent,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+    IonButton,
+    IonText,
+  ],
 })
 export class HistoricoPage implements OnInit {
+  medicoes: MedicaoResposta[] = [];
+  mensagemErro = '';
 
-  constructor() { }
+  constructor(private readonly relatorioService: RelatorioService) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.carregarHistorico();
   }
 
+  carregarHistorico(): void {
+    this.mensagemErro = '';
+
+    this.relatorioService.obterHistorico().subscribe({
+      next: (resposta) => {
+        this.medicoes = resposta.medicoes ?? [];
+      },
+      error: () => {
+        this.mensagemErro = 'Não foi possível carregar o histórico.';
+      },
+    });
+  }
 }
--- src/app/relatorios/services/relatorio.service.ts
+++ src/app/relatorios/services/relatorio.service.ts
@@ -1,8 +1,33 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { environment } from '../../../environments/environment';
+import { UsuarioService } from '../../usuarios/services/usuario.service';
+import { HistoricoMedicoesResposta } from '../models/historico-medicoes-resposta.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Relatorio {
-  
+export class RelatorioService {
+  private readonly apiUrl = `${environment.apiUrl}/relatorios`;
+
+  constructor(
+    private readonly http: HttpClient,
+    private readonly usuarioService: UsuarioService
+  ) {}
+
+  obterHistorico(): Observable<HistoricoMedicoesResposta> {
+    return this.http.get<HistoricoMedicoesResposta>(`${this.apiUrl}/historico`, {
+      headers: this.obterHeadersAutenticados(),
+    });
+  }
+
+  private obterHeadersAutenticados(): HttpHeaders {
+    const token = this.usuarioService.obterToken();
+
+    return new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+  }
 }
