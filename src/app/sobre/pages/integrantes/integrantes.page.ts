b753348731bf28ac82cba155111f08398bc14ffd
Laura Perlingeiro <laura.bentper@gmail.com>
2026-06-16 16:34:09 -0300

    feat(sobre): criar tela de integrantes

--- src/app/sobre/pages/integrantes/integrantes.page.ts
+++ src/app/sobre/pages/integrantes/integrantes.page.ts
@@ -1,20 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Component } from '@angular/core';
+import {
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardTitle,
+  IonContent,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-integrantes',
   templateUrl: './integrantes.page.html',
   styleUrls: ['./integrantes.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+  ],
 })
-export class IntegrantesPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class IntegrantesPage {}
